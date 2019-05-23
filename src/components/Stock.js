14fa2ba0ffaa625159a41c4adc757cd96a3f58c2
alph4x <danisharora099@gmail.com>
2019-05-24 00:56:14 +0530

    a

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -6,8 +6,20 @@ export class Stock extends React.Component {
         super();
         this.state = {
             fishStock: [
-
-            ],
+                {
+                    species: "Tuna",
+                    image: "https://5.imimg.com/data5/UE/XF/MY-60738954/fresh-tuna-fish-250x250.jpg",
+                    price: 100,
+                    amount: 50,
+                    fishes: ["a", "b"]
+                }, {
+                    species: "Cod",
+                    image: "https://4.imimg.com/data4/EY/IM/MY-5559191/reef-cod-fish-500x500.jpg",
+                    price: 200,
+                    amount: 20,
+                    fishes: ["a", "b"]
+                }
+            ]
         }
     }
 
